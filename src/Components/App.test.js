108a10ac5438e82ea8351c23ddb460333a546383
Marjorie Mejia <mm035359@gmail.com>
2021-05-07 22:27:34 -0400

    no test available yet

--- src/Components/App.test.js
+++ src/Components/App.test.js
@@ -1,8 +1,1 @@
-// import { render, screen } from '@testing-library/react';
-// import App from './DonationForm';
-
-// test('renders learn react link', () => {
-//   render(<App />);
-//   const linkElement = screen.getByText(/learn react/i);
-//   expect(linkElement).toBeInTheDocument();
-// });
+//Test not available yet
