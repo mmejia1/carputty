8d7bd9b1cf2c88ac43f335c98b70b56402dcf639
Marjorie Mejia <mm035359@gmail.com>
2021-05-07 21:56:43 -0400

    fe:cleaned up code

--- src/Components/DonationForm.js
+++ src/Components/DonationForm.js
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-
 import {
   Button,
   Card,
@@ -11,12 +10,9 @@ import {
   Col,
 } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
-//import ProgressBar from 'react-bootstrap/ProgressBar';
 
 function DonationForm() {
-  //info for bar component
-  const [totalAmount, setTotalAmount] = useState(5000);
-  //for checking that the amount submitted is 5 || greater
+  const [totalAmountNeeded, setTotalAmount] = useState(5000);
   const [amountRaised, setAmountRaised] = useState(0);
   const [donors, setDonators] = useState(0);
   const [inputAmount, setInputAmount] = useState('');
@@ -24,58 +20,42 @@ function DonationForm() {
   const [weHitTarget, setWeHitTarget] = useState(false);
   const [isNum, setIsNum] = useState(false);
 
-  //function setTotalAmount() {}
-
   function handleSubmit(evt) {
     evt.preventDefault();
-    //const stringAmount = inputAmount;
-    console.log('MADE IT HERE', inputAmount);
+    //checking input and making sure its a number and not a letter
     if (isNaN(inputAmount)) {
       setIsNum(true);
-      //what is this doing
+      //we reset tooLow in case it was triggered
       setTooLow(false);
-
-      console.log('in set imput');
     }
-
     setInputAmount('');
   }
 
   function onChange(evt) {
-    console.log(evt.target.value);
-    let num = inputAmount.slice();
-    console.log(num);
-    num = evt.target.value;
-    console.log('numArray', num);
-    console.log(typeof amountRaised);
+    let num = evt.target.value;
     setInputAmount(num);
   }
   function handleSum() {
     //here well do our check for $5
-
+    //we input nums as strings '55' we need it as a num
     if (Number(inputAmount) > 5) {
+      //In case these cases were triggered previously we need to reset them
       setIsNum(false);
       setTooLow(false);
-      console.log('inputamount', inputAmount);
-      const amountNeeded = totalAmount - inputAmount;
-      console.log(typeof totalAmount);
+
+      const amountNeeded = totalAmountNeeded - inputAmount;
       let amount = amountRaised + Number(inputAmount);
-      console.log('amount', amount);
-      console.log(amountNeeded);
-      //add donor
       let numberOfDonors = donors + 1;
       setTotalAmount(amountNeeded);
-      console.log('numberOfDonors', numberOfDonors);
       setDonators(numberOfDonors);
       setAmountRaised(amount);
     }
-    if (Number(inputAmount) > Number(totalAmount)) {
+    if (Number(inputAmount) > Number(totalAmountNeeded)) {
       setWeHitTarget(true);
       let numberOfDonors = donors + 1;
       setDonators(numberOfDonors);
     }
     if (Number(inputAmount) < 5) {
-      console.log('OOPS!');
       setIsNum(false);
       setTooLow(true);
     }
@@ -88,7 +68,7 @@ function DonationForm() {
           <Alert class='alert alert-primary text-center' role='alert'>
             {weHitTarget && <p>we did it!</p>}{' '}
             {!weHitTarget && (
-              <p>${totalAmount} still needed to fund this project</p>
+              <p>${totalAmountNeeded} still needed to fund this project</p>
             )}
           </Alert>
 
@@ -105,7 +85,6 @@ function DonationForm() {
                   data-placement='top'
                   now={(amountRaised / 5000) * 100}
                   variant='info'
-                  // label={(amountRaised / 5000) * 100}
                 ></ProgressBar>
                 <Card.Title className='mt-5 mb-5'>
                   <h2>Only a few days left to fund this project</h2>
@@ -115,7 +94,6 @@ function DonationForm() {
                   project
                 </p>
                 <Form onSubmit={handleSubmit}>
-                  {/* <Form.Group controlId='formHorizontal'> */}
                   <div className='App'>
                     <label htmlFor='amount'></label>
                     <Form.Row>
